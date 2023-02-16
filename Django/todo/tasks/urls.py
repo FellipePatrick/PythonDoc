092d57efb34839b0e12f07f162eb16a81f03f846
pokytester <fellipe.patrick678@gmail.com>
2023-02-16 11:23:48 -0300

    task view

--- Django/todo/tasks/urls.py
+++ Django/todo/tasks/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('helloword/', views.helloword),
     path('', views.taskList, name='task-list'),
+    path('task/<int:id>', views.taskView, name='task-view'),
 ]
--- Django/todo/tasks/views.py
+++ Django/todo/tasks/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from django.http import HttpResponse
 
-
-def helloword(request):
-    return HttpResponse('Hello Word!')
+from .models import Task
 
 def taskList(request):
-    return render(request, 'index/index.html')
+    tasks = Task.objects.all()
+    return render(request, 'index/index.html', {'tasks':tasks})
+
+def taskView(request, id):
+    tasks = get_list_or_404(Task, pk=id)
+    return render(request, 'tasks/task.html', {'tasks':tasks} )
 
 # Create your views here.
